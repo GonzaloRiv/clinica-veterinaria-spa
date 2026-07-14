f5e951a35d1579b60409874d7cad81bb1d25c5f9
Gonzalo Israel Rivera <gonzalo.rivera40@gmail.com>
2026-07-13 23:26:44 -0400

    feat(): Se agrega etiqueta de google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,6 +46,21 @@ export default function RootLayout({
         {children}
         <Analytics />
       </body>
+
+      {/* Google Ads / Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-18311444683"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-ads" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-18311444683');
+        `}
+      </Script>
     </html>
   )
 }
