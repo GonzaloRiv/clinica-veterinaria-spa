c7f07933cead146956e92092747450fb324c1504
Gonzalo Israel Rivera <gonzalo.rivera40@gmail.com>
2026-07-13 23:27:57 -0400

    feat(): Se agrega import de la etiqueta Script de nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { DM_Sans, Playfair_Display } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
+import Script from 'next/script'
 
 const _dmSans = DM_Sans({ subsets: ['latin'], variable: '--font-dm-sans' })
 const _playfair = Playfair_Display({ subsets: ['latin'], variable: '--font-playfair' })
